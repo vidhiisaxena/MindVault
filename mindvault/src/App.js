9f72b6e1a746e900edb402539003d78d2b183882
Vidhi Saxena <vidhi094btmae23@igdtuw.ac.in>
2025-02-21 23:55:55 +0530

    Added Chatbot

--- mindvault/src/App.js
+++ mindvault/src/App.js
@@ -4,6 +4,7 @@ import LandingPage from "./components/landing/landing";
 import LoginPage from "./components/signup/login"; 
 import Dashboard from "./components/dashboard/Dashboard";
 import FlashcardList from "./components/flashcardlist";
+import { useEffect } from "react";
 
 function App() {
   return (
@@ -15,6 +16,29 @@ function App() {
         <Route path="/flashcardlist" element={<FlashcardList />} />
       </Routes>
     </Router>
+  );
+
+{/* <script src="https://cdn.botpress.cloud/webchat/v2.2/inject.js"></script>
+<script src="https://files.bpcontent.cloud/2025/02/21/17/20250221173635-6DOLLHY3.js"></script> */}
+    
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "https://cdn.botpress.cloud/webchat/v2.2/inject.js";
+    script.src =
+      "https://files.bpcontent.cloud/2025/02/21/17/20250221173635-6DOLLHY3.js";
+    script.async = true;
+    document.body.appendChild(script);
+    
+    return () => {
+      document.body.removeChild(script); // Cleanup on unmount
+    };
+  }, []);
+
+  return (
+    <div>
+      <h1>Welcome to My Website</h1>
+      {/* Your website content */}
+    </div>
   );
 }
 
